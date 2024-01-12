50b546b21cc3c28557f5e9b79c737dc4cbdb80a4
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-01-12 13:05:10 +0000

    Update plotting_outlier_heatmap.R

--- GWAS/GWAS_Single_SP/plotting_outlier_heatmap.R
+++ GWAS/GWAS_Single_SP/plotting_outlier_heatmap.R
@@ -1,152 +1,184 @@
-# Packages 
-library(ggplot2)
+##############################
+# Install required libraries #
+##############################
+#install.packages("ggfortify")
+library(ggfortify)
+#install.packages("qqman")
+library(qqman)
+library(tidyverse)
+#install.packages("ggtext")
+library(ggtext)
+#devtools::install_github("norment/normentR")
+library(normentR)
+library(grid)
+library(gridExtra)
 library(dplyr)
-library(reshape2)
-library("gplots")
-library(wesanderson)
-library(gtools)
-library(ComplexHeatmap)
+#install.packages("sjlabelled")
+library("sjlabelled")
+library(ggplot2)
+library(cowplot)
+#remotes::install_github("HanjoStudy/quotidieR")
+library(quotidieR)
+#install.packages("ggthemes")
+library("ggthemes")
+library("gggenes")
 
-# read data 
-args = commandArgs(trailingOnly=TRUE)
-Path_Data = args[1]
-data = read.table(Path_Data, colClasses = c(rep("factor",5)))
+###############
+# Import data #
+###############
+dat = commandArgs(trailingOnly=TRUE)
+list = list() #  Create an empty list that will contain all the plots
+counter = 1 #  Will count the element in the list
 
-# Prepare data in the right format
-data = data[,-c(1)]
-colnames(data) = c("Sample_name","Phenotype","Genotype","Species","CHR","SNP")
-data$Genotype = gsub(pattern = "/", x = data$Genotype,replacement = "",perl = TRUE)
-data$Genotype = gsub(pattern = "\\|", x = data$Genotype,replacement = "",perl = TRUE)
+for (sp in dat){
+	cat(paste("SPECIES: ",sp,sep=""),"\n")
+	gwas = read.table(paste("/mnt/scratch/projects/biol-specgen-2018/yacine/Conv_Evol/GWAS/Figure_2/Data/",sp,".txt",sep=""),skip=1)
+	bonf_threshold = -log10(0.05/as.numeric(dim(gwas)[1]))
+	
+	################          
+	# Prepare data #
+	################
+	colnames(gwas) = c("CHR","SNP","BP","P")
+	order_contigs = read.table(paste("/mnt/scratch/projects/biol-specgen-2018/yacine/Conv_Evol/GWAS/Figure_2/Data/scaffold_order_",sp,".txt",sep=""))
+	order_contigs = as.data.frame(order_contigs)
+	order_contigs = as.factor(order_contigs$V1)
+	gwas$CHR=factor(gwas$CHR, levels=order_contigs)
+	sig_data <- gwas %>% subset(P < 1)
+	notsig_data <- gwas %>% subset(P >= 1) %>% group_by(CHR) %>% sample_frac(0.01)
+	gwas_data <- bind_rows(sig_data, notsig_data)
 
-# Read color data
-Path_Data_Color = args[2]
-color = read.table(Path_Data_Color,header = TRUE,comment.char='@',encoding="UTF-8")
-couleur = color[unique(data$Species) %in% color$Species,]
-data$Species = gsub(pattern = "(\\w+)_(\\w+)", x = data$Species,replacement = "\\2",perl = TRUE)
-data$CHR = gsub(pattern = "SUPER_", x = data$CHR,replacement = "Chr ",perl = TRUE)
-data = data[order(data$Species),]
-couleur$Species = gsub(pattern = "(\\w+)_(\\w+)", x = color$Species,replacement = "\\2",perl = TRUE)
+	data_cum <- gwas_data %>% 
+	group_by(CHR) %>% 
+	summarise(max_bp = max(BP)) %>% 
+	mutate(bp_add = lag(cumsum(max_bp), default = 0)) %>% 
+	select(CHR, bp_add)
 
-# Extract information about the phenotype for the heatmap annotation
-a = table(data$Phenotype)/length(unique(data$SNP))
-a = a[a!=0]
-list=list()
-list2=list()
-counter = 1
-color = c("orange","sienna","darkgoldenrod1")
+	gwas_data <- gwas_data %>% 
+	inner_join(data_cum, by = "CHR") %>% 
+	mutate(bp_cum = BP + bp_add)
 
-for (i in 1:length(a)){
-  list[[counter]]= rep(names(a[i]),a[i])
-  bizbizfaitlamouche_leretour = color[counter]
-  names(bizbizfaitlamouche_leretour)= c(names(a[i]))
-  list2[[counter]]= bizbizfaitlamouche_leretour
-  counter = counter + 1
+	axis_set <- gwas_data %>% 
+	group_by(CHR) %>% 
+	summarize(center = mean(bp_cum))
+
+	ylim <- gwas_data %>% 
+	filter(P == min(P)) %>% 
+	mutate(ylim = abs(floor(log10(P))) + 2) %>% 
+	pull(ylim)	
+	
+	#####################################          
+	# Extract scaffold larger than 2 Mb #
+	#####################################
+	list2 = list()
+	a = 1
+	for (i in unique(gwas_data$CHR)){
+		Size = length(gwas_data[gwas_data$CHR==i,1])
+		print(Size)
+		if(Size > 10000){
+			list2[[a]] = i
+			a = a + 1
+		}
+	}
+
+	Big_scaff = do.call(c,list2)
+	gwas_data = gwas_data[gwas_data$CHR %in% Big_scaff,]
+
+	########################################################################
+        # Find scaffold with the peak and the associated points above the peak #
+        ########################################################################
+	thres_pval = 0.05/dim(gwas)[1]
+	min(unlist(gwas_data$P))
+	peak = gwas_data[gwas_data$P==min(unlist(gwas_data$P)),1]
+	peak_position = gwas_data[gwas_data$P==min(unlist(gwas_data$P)),3]
+	#peak = names(sort(table(gwas_data[gwas_data$P < thres_pval,1]),decreasing=T)[1])
+
+	####################
+	# Plot per species #
+	####################
+	 p <- ggplot(gwas_data, aes(x = bp_cum/1000000, y = -log10(P),color =CHR)) +
+	 geom_point() +
+	 scale_x_continuous() +
+	 scale_color_manual(values = rep(c("gray", "black"), unique(length(axis_set$CHR)))) +
+	 labs(x = "Genome position (Mb)", y = "-log<sub>10</sub>(p)") + 
+	 theme_bw() +
+	 theme(legend.position = "none",panel.grid.major.x = element_blank(),panel.grid.minor.x = element_blank(),axis.title.x = element_markdown(size=12),axis.title.y = element_markdown(size=12),plot.title = element_text(hjust = 0.5,size=12), text = element_text(size=14)) +
+	 geom_hline(yintercept=bonf_threshold, linetype="dashed", color = "orange", size=0.8) + 
+	 geom_point(data = gwas_data[gwas_data$CHR==peak & -log10(gwas_data$P) > bonf_threshold & gwas_data$BP > (peak_position - 500000) & gwas_data$BP < (peak_position + 500000),], aes(x= bp_cum/1000000, y = -log10(P)),colour="orange") +
+	 ggtitle(sp) 
+	
+	png(file=paste(sp,"_Cortex_GWAS.png",sep=""),width=600,height=400,type="cairo")
+	plot(p)
+	dev.off()
+	
+	#########################
+        # GWAS zoom around peak #
+        #########################
+	region = read.table("/mnt/scratch/projects/biol-specgen-2018/yacine/Conv_Evol/GWAS/Figure_2/Data/Genes_positions.txt")
+	region = region[region$V1==sp,]
+	colnames(region) = c("Species","Scaffold","Gene","Start","Stop","Size","orientation")
+	start_plot = min(unlist(region[,c(4,5)]))
+	end_plot = max(unlist(region[,c(4,5)]))
+	gwas_zoom = gwas_data[gwas_data$CHR==peak & gwas_data$BP > (start_plot-25000) & gwas_data$BP < (end_plot+25000),]
+
+	p_zoom <- ggplot(gwas_zoom, aes(x = BP/1000, y = -log10(P))) +
+        geom_point(color="gray") +
+        scale_x_continuous() +
+        labs(x = "Genome position (kb)", y = "-log<sub>10</sub>(p)") +
+        theme_bw() +
+        theme(legend.position = "none",panel.grid.major.x = element_blank(),panel.grid.minor.x = element_blank()) +
+	theme(axis.title.x=element_blank()) +
+	theme(axis.title.y = element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank(),plot.margin=unit(c(0.8,1,-1.2,1), "cm")) +
+        geom_hline(yintercept=bonf_threshold, linetype="dashed", color = "orange", size=0.8)
+
+	############ 
+        # Syntheny #
+        ############ 
+	region = region[,-c(2)]
+	region$New_start = round((region$Start + region$Stop)/2 - (region$Size/2),digits = 0)
+	region$New_end = round((region$Start + region$Stop)/2 + (region$Size/2),digits = 0)
+	
+	region[region$orientation=="+",6] = TRUE
+        region[region$orientation=="-",6] = FALSE
+
+	dummies <- make_alignment_dummies(region,aes(xmin = New_start, xmax = New_end, y = Species, id = Gene, forward=orientation),on = "parn")
+	
+	p_syntheny <- ggplot(region, aes(xmin = New_start , xmax = New_end,y = Species, fill = Gene, forward = orientation)) +
+	geom_gene_arrow()  +
+	scale_color_manual(values = c("black")) + scale_fill_manual(values = c("gold","azure3","coral","forestgreen","magenta1","black")) + guides(color = "none")+
+	theme(legend.position="bottom",axis.title.x=element_blank(),axis.text.x=element_blank(), axis.ticks.x=element_blank(),axis.title.y=element_blank(), axis.text.y=element_blank()) + 
+	theme(panel.border = element_blank()) +
+	theme(panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "white"),panel.grid.major.x = element_blank(), panel.grid.major.y = element_line(colour="black"),
+        legend.margin=margin(0,0,0,0),
+        legend.box.margin=margin(-10,0,160,0), plot.margin=unit(c(1,1,-0.5,1), "cm")) 
+	
+	######################################  
+        # Plot syntheny and GWAS around peak #
+        ######################################
+	png(file=paste(sp,"_Zoom_GWAS.png",sep=""),width=700,height=500)
+        grid.arrange(p_zoom,p_syntheny)
+        dev.off()
+	
+	#tiff(file=paste(sp,"_Zoom_GWAS.tif",sep=""), res=300)
+	#grid.arrange(p_zoom,p_syntheny)
+        #dev.off()
+	############################
+        # Move to the next species #
+        ############################
+	list[[counter]] = p
+	counter = counter + 1
 }
 
+###########################################
+# Combine all the GWAS in a single figure #
+###########################################
+#png(file="Cortex_GWAS.png",width=600,height=1200,type="cairo")
+#do.call(grid.arrange,c(list,ncol=1))
+#dev.off()
 
-# Extract information about the species for the heatmap annotation
-list3=list()
-list4=list()
-b = table(data$Species)/length(unique(data$SNP))
-pheno1 = unique(data[which(data$Phenotype=="2"),4])
-pheno2 = unique(data[which(data$Phenotype=="3"),4])
-pheno_intermediate = unique(data[which(data$Phenotype=="2.5"),4])
-b = c(b[pheno1],b[pheno_intermediate],b[pheno2])
-counter = 1
-
-for (i in 1:length(b)){
-  list3[[counter]]= rep(names(b[i]),b[i])
-  bizbizfaitlamouche_leretour = couleur[couleur$Species == names(b[i]),2]
-  names(bizbizfaitlamouche_leretour)= c(names(b[i]))
-  list4[[counter]]= bizbizfaitlamouche_leretour
-  counter = counter + 1
-}
-
-
-
-# Extract information about the CHROMOSOME for the heatmap annotation
-chr = table(data$CHR)/length(unique(data$Sample_name))
-list7=list()
-list8=list()
-counter = 1
-color = rep("black",length(chr))
-
-for (i in 1:length(chr)){
-  list7[[counter]]= rep(names(chr[i]),chr[i])
-  bizbizfaitlamouche_leretour = color[counter]
-  names(bizbizfaitlamouche_leretour)= c(names(chr[i]))
-  list8[[counter]]= bizbizfaitlamouche_leretour
-  counter = counter + 1
-}
-
-# Chromosome information for plotting
-chromosome_tab = table(data$CHR)/length(unique(data$Sample_name))
-list_chr = list()
-counter = 1
-for (i in 1:length(chromosome_tab)){
-  list_chr[[counter]] = rep(names(chromosome_tab[i]),chromosome_tab[i])
-  counter = counter + 1
-}
-
-chromo_plot = do.call(c,list_chr)
-chromo_plot = mixedsort(chromo_plot)
-
-
-# Extract information about the SNPS for the heatmap annotation
-pos = table(unique(data$SNP))
-list5=list()
-list6=list()
-counter = 1
-color = rainbow(length(unique(data$SNP)))
-
-for (i in 1:length(pos)){
-  list5[[counter]]= rep(names(pos[i]),pos[i])
-  bizbizfaitlamouche_leretour = color[counter]
-  names(bizbizfaitlamouche_leretour)= c(names(pos[i]))
-  list6[[counter]]= bizbizfaitlamouche_leretour
-  counter = counter + 1
-}
-
-# reorder data so species fits the genotype
-list_sp = list()
-counter = 1
-for (i in names(b)){
-  list_sp[[counter]] = data[data$Species==i,]
-  counter = counter + 1
-}
-data = as.data.frame(do.call(rbind, list_sp))
-
-
-
-# Information plotted in the heatmap
-list_snps = list()
-counter = 1
-for (i in unique(data$SNP)){
-  list_snps[[counter]] = data[data$SNP==i,3]
-  counter = counter + 1
-}
-Input = as.matrix(do.call(cbind, list_snps))
-
-# Set the specific annotation block used by complexheatmap
-Prefix = args[3]
-Number_pheno = length(levels(data$Phenotype))
-
-if(Number_pheno==2){
-	Phenotypes_names = c(Prefix, paste("No ",Prefix, sep = ""))
-	ha_row = rowAnnotation(Pheno=do.call(c,list), Species = do.call(c,list3), col = list(Pheno = do.call(c,list2), Species =  do.call(c,list4)),border = TRUE,annotation_legend_param = list(Pheno = list(labels = Phenotypes_names)))
-}else{
-	Phenotypes_names = c(Prefix, paste("No ",Prefix, sep = ""),"Hybrid")
-	ha_row = rowAnnotation(Pheno=do.call(c,list), Species = do.call(c,list3), col = list(Pheno = do.call(c,list2), Species =  do.call(c,list4)),border = TRUE,annotation_legend_param = list(Pheno = list(labels = Phenotypes_names)))
-}
-
-ha_column = HeatmapAnnotation(Chromosome = do.call(c,list7), col = list( Chromosome = do.call(c,list8)),border = TRUE)
-
- 
-# Define color code
-pal <- wes_palette("Zissou1", length(unique(data$Genotype)), type = "continuous")
-pal = c("white",pal[c(1,3,4)])
-
-# Plot heatmap
-pdf(paste(Prefix,"_GWAS_heatmap.pdf",sep=""),7,8)
-Heatmap(Input,show_row_dend=FALSE,show_column_dend=FALSE,name = "zygosity",show_row_names = FALSE,use_raster=FALSE, row_title_gp = gpar(fontsize = 10), bottom_annotation = ha_column, left_annotation = ha_row,row_split= factor(do.call(c,list)), column_split = factor(chromo_plot,levels = mixedsort(levels(factor(chromo_plot)))), col = pal,border = TRUE)
-dev.off()
+###########################################
+# Combine all the GWAS in a single figure #
+###########################################
+ png(file="Optix_GWAS.png",width=600,height=700,type="cairo")
+ do.call(grid.arrange,c(list,ncol=1))
+ dev.off()
