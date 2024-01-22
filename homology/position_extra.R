6c1bbbade002e0b507fa2d6e75dfed5159c51954
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-01-22 19:07:19 +0000

    Update position_extra.R

--- homology/position_extra.R
+++ homology/position_extra.R
@@ -2,7 +2,7 @@ dat = commandArgs(trailingOnly=TRUE)
 data = read.table(dat)
 
 scaffold = data$V1[1]
-start = data$V2[2] - 150000
-end = data$V2[2] + 150000
+start = data$V2[2] - 25000
+end = data$V2[2] + 25000
 
 cat(paste(scaffold, start, end, sep="\t"),"\n")
