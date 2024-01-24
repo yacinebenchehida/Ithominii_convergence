7d2e467bb5f135e750f287b90fa17227451f5086
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-01-24 15:14:01 +0000

    Update import_data.sh

--- Annotation/import_data.sh
+++ Annotation/import_data.sh
@@ -11,9 +11,17 @@
 EDIRECT="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/edirect"
 module load SRA-Toolkit/3.0.3-gompi-2022a
 module load pigz/2.7-GCCcore-12.2.0
-SAMPLES=$($EDIRECT/esearch -db sra -query PRJNA725991 | $EDIRECT/efetch -format runinfo | cut -d "," -f 1,5,8,29|perl -pe 's/,/\t/g'|awk 'NR > 1 {print $1}')
+
+# Figure out samples to download
+SAMPLES=$($EDIRECT/esearch -db sra -query PRJEB60199 | $EDIRECT/efetch -format runinfo | cut -d "," -f 1,5,8,29|perl -pe 's/,/\t/g'|awk 'NR > 1 {print $1}'|grep -E "ERR11837472$
 echo $SAMPLES
 
+# Download sample and transform them into R1 and R2 reads
 for i in $SAMPLES; do echo $i; fastq-dump --split-files $i; done
-for i in *fastq; do fastq-dump --split-3 $i; done
+
+# Combine the R1 reads of all samples into a single file (same for R2).
+for i in *_1.fastq; do echo $i; cat $i >> RNA_seq_Melinaea_R1.fastq ; done
+for i in *_2.fastq; do echo $i; cat $i >> RNA_seq_Melinaea_R2.fastq ; done
+
+# gzip the R1 and R2 fastq files
 for i in RNA*; do pigz -d -p 12 $i; done
