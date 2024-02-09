ddb7b42c5b6aad0238d3702f97b5559c80c3dce2
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-02-09 20:28:14 +0000

    Create fasta_combine.sh

---
+++ Phylogeny/ASTRAL/ANGSD/fasta_combine.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --mem=10GB
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --account=BIOL-SPECGEN-2018
+#SBATCH --job-name=fasCmb
+#SBATCH --time=0-00:15:00
+
+module load Biopython/1.81-foss-2022b
+
+RESULTS="/mnt/scratch/projects/biol-specgen-2018/yacine/Conv_Evol/ASTRAL/Results"
+cd $RESULTS
+for i in *
+do
+	cd $i
+	touch "$i"_combined.fasta
+	for fasta in *gz
+	do
+		id=$(echo $fasta|perl -pe 's/_'"$i"'.fa.gz//g')
+		python3 /mnt/scratch/projects/biol-specgen-2018/yacine/Conv_Evol/ASTRAL/Scripts/Add_sample_id_to_header.py $fasta $i > "$id"_"$i".fas
+		zcat "$id"_"$i".fas >> "$i"_combined.fasta
+		rm $fasta "$id"_"$i".fas
+	done
+	cd $RESULTS
+done 
