f06eaa03fec347766e8cd615ec080d62438c10a9
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-02-15 20:36:42 +0000

    Create ASTRAL.sh

---
+++ Phylogeny/ASTRAL/ANGSD/ASTRAL.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --mem=5GB
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --account=BIOL-SPECGEN-2018
+#SBATCH --job-name=ASTRAL
+#SBATCH --time=0-10:50:00
+
+##########################
+# load necessary modules #
+##########################
+module load Java/13.0.2
+
+##############
+# Run ASTRAL #
+##############
+java -Djava.library.path=/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/ASTRAL/lib/ -jar  /mnt/scratch/projects/biol-specgen-2018/yacine/Tools/ASTRAL/astral.5.7.3.jar -i ../Results/curated_combined_phylogeny.txt -o ../Results/astral_results -t 2 2> ../Results/astral_results.log
+[
