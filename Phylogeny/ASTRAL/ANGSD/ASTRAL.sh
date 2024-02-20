a2404aabaaf546fad999da57bea822a73caae7c8
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-02-20 09:06:20 +0000

    Update ASTRAL.sh

--- Phylogeny/ASTRAL/ANGSD/ASTRAL.sh
+++ Phylogeny/ASTRAL/ANGSD/ASTRAL.sh
@@ -6,17 +6,24 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --account=BIOL-SPECGEN-2018
 #SBATCH --job-name=ASTRAL
-#SBATCH --time=0-4:00:00
+#SBATCH --time=0-10:50:00
 
 ##########################
 # load necessary modules #
 ##########################
 module load Java/13.0.2
+module load module load Armadillo/12.6.2-foss-2023a
+PXRR="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/phyx/src/pxrr"
 
 ##############
 # Run ASTRAL #
 ##############
-java -Djava.library.path=/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/ASTRAL/lib/ -jar  /mnt/scratch/projects/biol-specgen-2018/yacine/Tools/ASTRAL/astral.5.7.3.jar -i ../Results/../Results/rooted_curated_combined_phylogeny.txt -o ../Results/astral_results -t 2 2> ../Results/astral_results.log
+java -Djava.library.path=/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/ASTRAL/lib/ -jar  /mnt/scratch/projects/biol-specgen-2018/yacine/Tools/ASTRAL/astral.5.7.3.jar -i ../Results/curated_combined_phylogeny.txt -o ../Results/astral_results -t 2 2> ../Results/astral_results.log
+
+########################
+# Root the astral tree #
+########################
+$PXRR -t ../Results/astral_results -g  Sample_11-2002-3511,Sample_2-2002-1553 -o ../Results/rooted_astral_results.txt
 
 ##########################
 # Plot final Astral tree #
