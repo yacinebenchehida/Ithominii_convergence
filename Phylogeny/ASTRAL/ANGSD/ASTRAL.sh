70c3a4806e57bf31ffd3b346ea685b44c6895065
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-02-20 09:10:20 +0000

    Update ASTRAL.sh

--- Phylogeny/ASTRAL/ANGSD/ASTRAL.sh
+++ Phylogeny/ASTRAL/ANGSD/ASTRAL.sh
@@ -12,7 +12,7 @@
 # load necessary modules #
 ##########################
 module load Java/13.0.2
-module load module load Armadillo/12.6.2-foss-2023a
+module load Armadillo/12.6.2-foss-2023a
 PXRR="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/phyx/src/pxrr"
 
 ##############
