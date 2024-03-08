6e90f202bf78fbc9b7c5673a21a6c2b60a77c517
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-03-08 16:20:55 +0000

    Update master.sh

--- SNPs_calling/7_phasing/master.sh
+++ SNPs_calling/7_phasing/master.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+# Author: Yacine Ben Chehida
+
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G 
+#SBATCH --account=BIOL-SPECGEN-2018 
+#SBATCH --job-name=master
+
 #############
 # SET PATHS #
 #############
-VCF="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/6_Combine_intervals/Results/$1/"$1".DP4_GQ5_QUAL5_F20.snps.vcf.gz"
+VCF="/mnt/scratch/projects/biol-specgen-2018/edd/H_anastasia/6_combine_intervals/Results/"$1".basic.filters.DP5_GQ10_Q10.snps.vcf.gz"
 RESULTS="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/9_Phasing/Results/$1"
 REF_PATH="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/0_Data/reference_genomes/$1"
 REF="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/0_Data/reference_genomes/$1"
@@ -11,7 +22,7 @@ FASTA_REF=$(ls $REF|grep -E "*.fa$|*.fasta$|*.fna$")
 # Load modules #
 ################
 module load HTSlib/1.15.1-GCC-11.3.0
-module load module load BCFtools/1.15.1-GCC-11.3.0
+module load BCFtools/1.15.1-GCC-11.3.0
 module load Biopython/1.81-foss-2022b
 
 ############################
@@ -33,7 +44,7 @@ array_number=$(cat order_scaff.txt|wc -l)
 ##################################
 # Run shapeit for each scaffolds #
 ##################################
-sbatch --array=1-$array_number ./shapeit.sh $1
+sbatch --array=1-$array_number ./shapeit.sh $1 $VCF
 
 ########################################### 
 # Merge all phased vcf into a single file #
