13f27de5350638173e564aaa1d90e03a90fafb6e
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-03-14 22:09:03 +0000

    Update Figure2.sh

--- GWAS/GWAS_Single_SP/Figure_2/Figure2.sh
+++ GWAS/GWAS_Single_SP/Figure_2/Figure2.sh
@@ -1,23 +1,24 @@
 #! /bin/bash
 
-#SBATCH --mem=25GB
+#SBATCH --mem=15GB
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --account=BIOL-SPECGEN-2018
 #SBATCH --job-name=GWAS_GEMMA
-#SBATCH --time=0-04:00:00
+#SBATCH --time=0-02:00:00
 
 module load R/4.2.1-foss-2022a
 module load Pillow/9.4.0-GCCcore-12.2.0
 
 # Generate plots
-Rscript Combine_GWAS_plots.R $@
+Rscript New_Combine_GWAS_plots.R $@
 
 # Crop png
 for i in $@; do echo $i; python ./crop_png.py "$i"_Zoom_GWAS.png Zoom_GWAS_"$i"; done
 
 # Crop pdf
+
 module purge
 module load PyPDF2/1.26.0-foss-2019b-Python-2.7.16
 
