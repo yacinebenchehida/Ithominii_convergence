ad5093632e0b6e3816f0f33ef9f584a1e9e7989f
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-05-29 17:20:01 +0100

    Update Figure2.sh

--- GWAS/GWAS_Single_SP/Figure_2/Figure2.sh
+++ GWAS/GWAS_Single_SP/Figure_2/Figure2.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#SBATCH --mem=15GB
+#SBATCH --mem=25GB
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
@@ -10,20 +10,30 @@
 
 module load R/4.2.1-foss-2022a
 module load Pillow/9.4.0-GCCcore-12.2.0
+module load BCFtools/1.15.1-GCC-11.3.0
 
 # Generate plots
-Rscript New_Combine_GWAS_plots.R $@
+Rscript New_Combine_GWAS_plots.R $@  2> warnings.txt
 
 # Crop png
-for i in $@; do echo $i; python ./crop_png.py "$i"_Zoom_GWAS.png Zoom_GWAS_"$i"; done
+#for i in $@; do echo $i; python ./crop_png.py "$i"_Zoom_GWAS.png Zoom_GWAS_"$i"; done
 
 # Crop pdf
 
 module purge
 module load PyPDF2/1.26.0-foss-2019b-Python-2.7.16
 
-for i in $@; do echo $i; python ./crop_pdf.py "$i"_Zoom_GWAS_features.pdf Zoom_GWAS_features_"$i"; done
+# Get all command line arguments into an array
+args=("$@")
+
+# Determine the index of the last element
+last_index=$((${#args[@]} - 1))
+
+# Loop over all elements except the last one
+for ((i = 0; i < last_index; i++)); do
+	echo ${args[i]}; python3 ./cropper.py "${args[i]}"_Zoom_GWAS.pdf Zoom_GWAS_"${args[i]}".pdf
+done
 
 # Move results to results folder 
-rm  "$i"_Zoom_GWAS.png "$i"_Zoom_GWAS_features.pdf 
-mv *png *pdf ../Results
+#rm  "$i"_Zoom_GWAS.png  #"$i"_Zoom_GWAS_features.pdf 
+mv *png *pdf ../Results/"${args[last_index]}"
