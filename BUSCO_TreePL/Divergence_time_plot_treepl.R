ca7272300c1176a4f0715f8e99cf42ac3428b061
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-06-06 12:27:37 +0100

    Update Divergence_time_plot_treepl.R

--- BUSCO_TreePL/Divergence_time_plot_treepl.R
+++ BUSCO_TreePL/Divergence_time_plot_treepl.R
@@ -4,7 +4,7 @@ library(phytools)
 library(strap)
 library(phyloch)
 
-annot_tree <- phyloch::read.beast("/Users/yacinebenchehida/Desktop/Convergent_evolution/All_Melinaea/TreePL/New/3_calibrations/Results/treeannotator_results.txt")
+annot_tree <- phyloch::read.beast("/Users/yacinebenchehida/Desktop/Convergent_evolution/All_Melinaea/TreePL/New/1_calibrations/Results/treeannotator_results_1_calibrations.txt")
 
 if (is.null(annot_tree$`CAheight_95%_HPD_MIN`)) {
   annot_tree$min_ages <- annot_tree$`height_95%_HPD_MIN`
@@ -17,8 +17,8 @@ if (is.null(annot_tree$`CAheight_95%_HPD_MIN`)) {
 annot_tree$root.time <- max(nodeHeights(annot_tree)) 
 
 
-pdf("busco_tree_treepl_3_calibrations.pdf",18,10)
-geoscalePhylo(ladderize(annot_tree, right = F), x.lim = c(0, 170), cex.tip = 0.6, cex.age = 1, cex.ts = 0.9, units = c("Epoch","Age"),quat.rm=TRUE,width= 2, align.tip.label=FALSE, ftype = "b")
+pdf("busco_tree_treepl_1_calibrations.pdf",18,10)
+geoscalePhylo(ladderize(annot_tree, right = F), x.lim = c(0, 190), cex.tip = 0.6, cex.age = 1, cex.ts = 0.9, units = c("Epoch","Age"),quat.rm=TRUE,width= 2, align.tip.label=FALSE, ftype = "b")
 
 T1 <- get("last_plot.phylo", envir = .PlotPhyloEnv)
 
