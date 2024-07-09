1d9d95a32ddc1d74362a2efcfc01fe10ca09b349
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-07-09 16:09:39 +0100

    Update selection_seq_interval.py

--- homology/selection_seq_interval.py
+++ homology/selection_seq_interval.py
@@ -2,27 +2,39 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
 import sys
-import os
 
-#usage: python -W ignore *fasta contigs start end 
+# Usage: python script.py input.fasta contig_name start end species_name
 
-input_seq_iterator = SeqIO.parse(open(sys.argv[1], "rU"), "fasta")
-start = int(sys.argv[3]) - 1 
-end =  int(sys.argv[4]) 
-sp = sys.argv[5]
+input_fasta = sys.argv[1]
+contig_name = sys.argv[2]
+start = int(sys.argv[3]) - 1  # Convert to 0-based index
+end = int(sys.argv[4])
+species_name = sys.argv[5]
 
+sign = end - start
+
+if(sign < 0):
+        tmp = start
+        start = end
+        end = tmp
+
+# Function to compute reverse complement using Biopython
 def reverse_complement(dna_sequence):
-    complement_dict = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-    reverse_comp_sequence = ''.join(complement_dict[base] for base in reversed(dna_sequence))
-    return reverse_comp_sequence
+	return str(Seq(dna_sequence).reverse_complement())
 
+# Read the input FASTA file
+input_seq_iterator = SeqIO.parse(open(input_fasta, "rU"), "fasta")
 
+# Iterate through sequences
 for record in input_seq_iterator:
-        if(record.id == sys.argv[2]):
-                if(sp == "Hypothyris_anastasia"):
-                        print(">"+ str(record.id))
-                        my_seq=record.seq[start:end]
-                        print(reverse_complement(my_seq.upper()))
-                else:
-                        print(">"+ str(record.id))
-                        print(record.seq[start:end])
+	if record.id == contig_name:
+		sequence = str(record.seq)
+		selected_sequence = sequence[start:end]
+
+		# Check if reverse complement is needed based on species name
+		if species_name in ("Hypothyris_anastasia", "Melinaea_mothone"):
+			print(f">{record.id}")
+			print(reverse_complement(selected_sequence.upper()))
+		else:
+			print(f">{record.id}")
+			print(selected_sequence)
