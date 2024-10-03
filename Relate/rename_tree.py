c1266cc51f0042a4da7b410c7f9b04edaa5f7957
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-10-03 11:47:31 +0100

    Update rename_tree.py

--- Relate/rename_tree.py
+++ Relate/rename_tree.py
@@ -1,24 +1,3 @@
-import sys
-
-# Function to generate intervals between numbers
-def generate_intervals(file_path):
-    with open(file_path, 'r') as file:
-        numbers = [int(line.strip()) for line in file]
-
-    for i in range(len(numbers) - 1):
-        start = numbers[i]
-        end = numbers[i + 1] - 1
-        print(f"{start} {end}")
-
-    # For the last number, add 1000
-    last_number = numbers[-1]
-    print(f"{last_number} {last_number + 1000}")
-
-# Main part
-if __name__ == "__main__":
-    file_path = sys.argv[1]
-    generate_intervals(file_path)
-(base) [ybc502@login2[viking2] Scripts]$ cat rename_tree.py
 from Bio import Phylo
 import sys
 import re  # Import the regex module
