5c871fa31287104d13f17de9b8c90ee36c231682
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-10-13 17:48:00 +0100

    Update master.sh

--- Twisst/master.sh
+++ Twisst/master.sh
@@ -1,29 +1,59 @@
-VCF="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/5_Filtering/Results/multisp/Melinaea_marsaeus/Marsaeus_no_invariants_all_variations.vcf.gz"
+VCF="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/5_Filtering/Results/multisp/Melinaea_marsaeus/multisp_Melinaea_marsaeus_genotypeGVCF.intervals_8.filters.DP4_GQ5_QUAL5.invariants.vcf.gz"
+#VCF="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/5_Filtering/Results/multisp/Melinaea_marsaeus/Marsaeus_no_invariants_all_variations.vcf.gz"
 GROUP="../Data/group_$1.txt"
 CHR="SUPER_2"
-P1="mothone"
-P2="satevis"
-P3="marsaeus_rileyi"
-P4="marsaeus_phasiana"
-PO="lilis"
-METHOD="NJ"
+START=24000000
+END=27000000
+TAXA="mothone,satevis,marsaeus_rileyi,marsaeus_phasiana"
+OUTPUT_PATH="."
+PREFIX="test"
+POP="/mnt/scratch/projects/biol-specgen-2018/yacine/Conv_Evol/Relate/Inputs/hindwing_black.txt"
+METHOD="ML"
 
-############################################
-# 1 - Run RAXML on each window of interest #
-############################################
-for ((i = 25956241, j = 25958240; i < 26031063 && j < 26031063; i = j + 1, j=j+2000))  ; do \
-#for ((i = 23956241, j = 23958240; i < 26031063 && j < 26031063; i = j + 1, j=j+2000))  ; do \
-sbatch ./Get_phylogenies.sh \
--v $VCF \
--c $CHR \
--s $i -e $j -f $GROUP \
--p1 $P1 -p2 $P2 -p3 $P3 -p4 $P4 -po $PO \
--m $METHOD \
--o ../Results/$1 -n "$CHR"_"$i"_"$j"_"$1"; \
-done
+###################################
+# 2' - SOFTWARE LAODING AND PATHS #
+###################################
+module load BCFtools/1.19-GCC-13.2.0
+module load R/4.2.1-foss-2022a
+module load VCFtools/0.1.16-GCC-11.2.0
+module load Biopython/1.83-foss-2023a
+SHAPEIT="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/shapeit4/bin/shapeit4.2"
+TWISST="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/twisst/twisst.py"
+SCRIPTS=$(pwd)
 
-#########################################
-# 2 - summarize the results with twisst #
-#########################################
-running_jobs1=$(squeue|grep ybc502| grep raxml| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
-sbatch  --dependency=aftercorr:$running_jobs1  ./twisst.sh  ../Results/$1 $P1 $P2 $P3 $P4 $PO $GROUP $METHOD
+##############################
+# 3 - Get the phenotype file #
+##############################
+# Create working directory
+if [ -d $OUTPUT_PATH/$PREFIX ]; then
+    rm -r $OUTPUT_PATH/$PREFIX
+fi
+mkdir -p $OUTPUT_PATH/$PREFIX
+
+# Create phenotype file
+if [ -f  $OUTPUT_PATH/$PREFIX/"$PREFIX"_phenotype_file.txt ]; then
+    rm  $OUTPUT_PATH/$PREFIX/"$PREFIX"_phenotype_file.txt
+fi
+touch  $OUTPUT_PATH/$PREFIX/"$PREFIX"_phenotype_file.txt
+
+SPECIES=$(echo $TAXA|perl -pe 's/,/ /g')
+echo $SPECIES
+for i in $SPECIES; do cat ${POP}| grep -P "\s$i\s*(\w)*\s*$" >>  $OUTPUT_PATH/$PREFIX/"$PREFIX"_phenotype_file.txt; done
+echo POP FILE READY
+
+
+#######################################################################
+# Create VCF for genomics regions of interest and species of interest #
+#######################################################################
+bcftools view --regions $CHR:$START-$END -S <(cat $OUTPUT_PATH/$PREFIX/"$PREFIX"_phenotype_file.txt|awk '{print $1}') $VCF > $OUTPUT_PATH/$PREFIX/"$PREFIX".vcf
+bgzip $OUTPUT_PATH/$PREFIX/"$PREFIX".vcf
+tabix $OUTPUT_PATH/$PREFIX/"$PREFIX".vcf.gz
+echo "SUBSETTED VCF FILE READY"
+
+# Create VCFs with 100 SNPs each
+echo "STARTED SPLITTING VCF INTO SMALLER VCF WITH 100 SNPS EACH"
+python3 ./splice.py $OUTPUT_PATH/$PREFIX/"$PREFIX".vcf.gz  $OUTPUT_PATH/$PREFIX/$PREFIX 100 $OUTPUT_PATH/$PREFIX/"$PREFIX".txt
+echo "SPLITTING FINISHED"
+
+# gzip and infer phylogeny for each vcf
+python3 ./Phylogenies_job_submitter.py $PREFIX $METHOD $OUTPUT_PATH/$PREFIX $OUTPUT_PATH/$PREFIX
