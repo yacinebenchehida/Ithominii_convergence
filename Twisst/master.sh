a3ff9f9695ef49fc54e22bf03dd0e8388aaf853c
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-10-15 15:54:54 +0100

    Update master.sh

--- Twisst/master.sh
+++ Twisst/master.sh
@@ -2,13 +2,16 @@ VCF="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/5_Filtering/R
 #VCF="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/5_Filtering/Results/multisp/Melinaea_marsaeus/Marsaeus_no_invariants_all_variations.vcf.gz"
 GROUP="../Data/group_$1.txt"
 CHR="SUPER_2"
-START=24000000
-END=27000000
+START=25000000
+END=26100000
 TAXA="mothone,satevis,marsaeus_rileyi,marsaeus_phasiana"
 OUTPUT_PATH="."
-PREFIX="test"
+PREFIX="test2"
 POP="/mnt/scratch/projects/biol-specgen-2018/yacine/Conv_Evol/Relate/Inputs/hindwing_black.txt"
-METHOD="ML"
+METHOD="NJ"
+PEAK_START="25965242"
+PEAK_END="26021063"
+INTROG_SP="marsaeus_rileyi,mothone"
 
 ###################################
 # 2' - SOFTWARE LAODING AND PATHS #
@@ -38,7 +41,7 @@ touch  $OUTPUT_PATH/$PREFIX/"$PREFIX"_phenotype_file.txt
 
 SPECIES=$(echo $TAXA|perl -pe 's/,/ /g')
 echo $SPECIES
-for i in $SPECIES; do cat ${POP}| grep -P "\s$i\s*(\w)*\s*$" >>  $OUTPUT_PATH/$PREFIX/"$PREFIX"_phenotype_file.txt; done
+for i in $SPECIES; do cat ${POP}| grep -P "\s$i\s*(\w)*\s*$"|awk '{print $1"\t"$2}'>>  $OUTPUT_PATH/$PREFIX/"$PREFIX"_phenotype_file.txt; done
 echo POP FILE READY
 
 
@@ -55,5 +58,18 @@ echo "STARTED SPLITTING VCF INTO SMALLER VCF WITH 100 SNPS EACH"
 python3 ./splice.py $OUTPUT_PATH/$PREFIX/"$PREFIX".vcf.gz  $OUTPUT_PATH/$PREFIX/$PREFIX 100 $OUTPUT_PATH/$PREFIX/"$PREFIX".txt
 echo "SPLITTING FINISHED"
 
-# gzip and infer phylogeny for each vcf
+#########################################
+# gzip and infer phylogeny for each vcf #
+#########################################
 python3 ./Phylogenies_job_submitter.py $PREFIX $METHOD $OUTPUT_PATH/$PREFIX $OUTPUT_PATH/$PREFIX
+
+#############################################################
+# Combine topologies into a single file + TWISST + plotting #
+#############################################################
+running_jobs1=$(squeue|grep whoami| grep phylo_job| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
+INTROGRESSED_TAXA1=$(echo ${INTROG_SP}|perl -pe 's/,/\t/g'|awk '{print $1}'|cut -d _ -f 2)
+echo $INTROGRESSED_TAXA1
+INTROGRESSED_TAXA2=$(echo ${INTROG_SP}|perl -pe 's/,/\t/g'|awk '{print $2}'|cut -d _ -f 2)
+echo $INTROGRESSED_TAXA2
+
+./run_twisst.sh $OUTPUT_PATH/$PREFIX $PREFIX $START $END $INTROGRESSED_TAXA1 $INTROGRESSED_TAXA2 $PEAK_START $PEAK_END
