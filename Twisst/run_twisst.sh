c5726a71f3e598515ef0549cf1e04260a70cc600
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-10-16 21:42:50 +0100

    Update run_twisst.sh

--- Twisst/run_twisst.sh
+++ Twisst/run_twisst.sh
@@ -1,4 +1,14 @@
+#!/bin/sh
+#SBATCH --time=01:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --account=BIOL-SPECGEN-2018 
+#SBATCH --job-name=twisst
+
 echo COMBINING ALL TREES INTO A SINGLE FILE
+SCRIPTS=$9
 
 ###################
 # Combine results #
@@ -33,8 +43,14 @@ done
 ##################################################
 # Remove lines genomics windows without topology #
 ##################################################
-grep -v -F -f missing_parts.txt topologies.txt > my_final_topologies.txt
-rm topologies.txt
+# Check if missing_parts.txt is empty
+if [[ ! -s missing_parts.txt ]]; then
+    # If missing_parts.txt is empty, output test_depenencies.txt unchanged
+    awk '{print $2"\t"$3}' "$2".txt  > positions.txt
+else
+    # If missing_parts.txt is not empty, proceed with the filtering
+    awk 'NR==FNR {remove[$1]; next} !($1 in remove)' missing_parts.txt "$2".txt  | awk '{print $2"\t"$3}' > positions.txt
+fi
 
 ##############
 # Run TWISST #
@@ -45,8 +61,20 @@ echo $G_OPTION
 echo G_OPTION CREATED
 
 # 2) RUN TWISST
+module load Biopython/1.83-foss-2023a
 TWISST="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/twisst/twisst.py"
-python $TWISST -t my_final_topologies.txt --method complete --groupsFile *phenotype* $G_OPTION --outputTopos Phylogeny.topos | gzip > Phylogeny.weights.tsv.gz
+python $TWISST -t topologies.txt --method complete --groupsFile *phenotype* $G_OPTION --outputTopos Phylogeny.topos | gzip > Phylogeny.weights.tsv.gz
+echo TWISST FINISHED
 
 # 3) Plot TWISST RESULTS
-Rscript $SCRIPTS/twisst_zscore.R $3 $4 0.020 $5 $6 $7 $8
+module purge
+module load  R/4.2.1-foss-2022a
+Rscript $SCRIPTS/twisst.R $3 $4 0.012 $5 $6 $7 $8
+echo TWISST RESULTS PLOTTED
+
+############
+# Cleaning #
+############
+rm -r "$2"_part_* *sh
+echo TEMPORARY FILES CLEANED.
+echo DONE
