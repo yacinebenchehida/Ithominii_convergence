f3fadfc99b9374181fbaf606a7825e00ca8ab58f
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2025-09-16 19:04:03 +0100

    Create f4.R

---
+++ Introgression/f4/f4.R
@@ -0,0 +1,18 @@
+library(admixtools)
+library(tidyverse)
+
+args <- commandArgs(trailingOnly = TRUE)
+data = "../Inputs/Inputs"
+subspecies_files <- args[1]
+pops <- read.table(subspecies_files)
+pop1 <- pops[1,1]
+pop2 <- pops[2,1]
+pop3 <- pops[3,1]
+pop4 <- pops[4,1]
+outfile = paste(args[2],"/",pop1,"_",pop2,"_",pop3,"_",pop4,".txt", sep="")
+print(outfile)
+
+out = f4(data, pop1, pop2, pop3, pop4)
+
+write_tsv(out, outfile)
+
