03f2a9a7668efa80d8722c6aad1d8862d5ccad2a
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2025-09-20 22:25:06 +0100

    Update f4.sh

--- Introgression/f4/f4.sh
+++ Introgression/f4/f4.sh
@@ -14,15 +14,36 @@ module load BCFtools/1.19-GCC-13.2.0
 module load  R/4.2.1-foss-2022a
 module load PLINK/1.90-beta-7.4-x86_64
 
+################
+# Script usage #
+################
+usage()
+{
+cat << EOF
+Usage: ./f4.sh -v <vcf> -o <outdir> -p <pop_file> --p1 <pop1> --p2 <pop2> --p3 <pop3> --p4 <pop4> -h
+
+OPTIONS:
+  -v, --vcf        Input VCF file name (full dataset to be subsetted). The VCF should be bgzipped and tabixed.
+  -o, --outdir     Output directory where results will be saved
+  -p, --pop        Tab separate population file containing population (col1) and sample ID (exactly as in the VCF)
+  --p1             Name of the first population (used as P1 in the f4 test)
+  --p2             Name of the second population (used as P2 in the f4 test)
+  --p3             Name of the third population (used as P3 in the f4 test)
+  --p4             Name of the fourth population (used as P4 in the f4 test)
+  -h               Show this usage information and exit
+EOF
+exit 0
+}
+
 ##########################
 # Parse input parameters #
 ##########################
-OPTIONS=$(getopt -o v:o:p: \
+OPTIONS=$(getopt -o v:o:p:h \
     --long vcf:,outdir:,pop:,p1:,p2:,p3:,p4: \
     -n "$0" -- "$@")
 
 if [ $? -ne 0 ]; then
-    exit 1
+    usage
 fi
 
 eval set -- "$OPTIONS"
@@ -36,11 +57,16 @@ while true; do
         --p2) P2="$2"; shift 2 ;;
         --p3) P3="$2"; shift 2 ;;
         --p4) P4="$2"; shift 2 ;;
+        -h) usage; shift ;;
         --) shift; break ;;
         *) break ;;
     esac
 done
 
+# Check mandatory parameters
+if [[ -z $VCF || -z $OUTDIR || -z $POP_FILE || -z $P1 || -z $P2 || -z $P3 || -z $P4 ]]; then
+    usage
+fi
 
 #################
 # Define prefix #
@@ -66,7 +92,7 @@ if [ -f  $RESULTS/"$PREFIX"_phenotype_file.txt ]; then
 fi
 touch  $RESULTS/"$PREFIX"_phenotype_file.txt
 
-for i in $P1 $P2 $P3 $P4; do cat ${POP_FILE} |awk -v pattern="$i" '$1 ~ pattern'  >>  $RESULTS/"$PREFIX"_phenotype_file.txt; done
+for i in $P1 $P2 $P3 $P4; do cat ${POP_FILE} | awk -v pattern="$i" '$1 ~ pattern' >> $RESULTS/"$PREFIX"_phenotype_file.txt; done
 for i in $P1 $P2 $P3 $P4; do echo $i >> $RESULTS/"$PREFIX"_subspecies.txt; done
 echo POP FILE READY
 
@@ -82,14 +108,12 @@ bcftools view -S <(awk '{print $2}' "$RESULTS/${PREFIX}_phenotype_file.txt") \
   
 tabix "$RESULTS/${PREFIX}.vcf.gz"
 echo Subsetted VCF generated
-# load plink
-#PLINK="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/plink_linux_x86_64_20231018/plink"
 
 ###############################################
 # Run plink (output needed to run admixtools) #
 ###############################################
 echo Running plink
-plink --vcf $VCF --double-id --allow-extra-chr --allow-no-sex --set-missing-var-ids @:# --make-bed --out $RESULTS/subspecies --threads 8
+plink --vcf "$VCF" --double-id --allow-extra-chr --allow-no-sex --set-missing-var-ids @:# --make-bed --out "$RESULTS/subspecies" --threads 8
 echo Change contig names to numbers
 awk '{if(!($1 in seen)) seen[$1]=++counter; $1=seen[$1]; print}' OFS='\t' $RESULTS/subspecies.bim > $RESULTS/Inputs.bim
 mv $RESULTS/subspecies.bed $RESULTS/Inputs.bed
@@ -101,4 +125,3 @@ echo Plink done
 echo Start f4 statistics using admixtools
 Rscript ./f4.R $RESULTS/"$PREFIX"_subspecies.txt $RESULTS
 echo DONE
-
