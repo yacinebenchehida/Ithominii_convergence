b0bebab8dd741b6f539aabc5c00c1075d0185653
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2025-09-20 21:59:54 +0100

    Update f4.sh

--- Introgression/f4/f4.sh
+++ Introgression/f4/f4.sh
@@ -2,29 +2,64 @@
 #SBATCH --time=00-01:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=35G 
 #SBATCH --account=BIOL-SPECGEN-2018 
 #SBATCH --job-name=f4
 
+################
+# Load modules #
+################
 module load BCFtools/1.19-GCC-13.2.0
+module load  R/4.2.1-foss-2022a
+module load PLINK/1.90-beta-7.4-x86_64
 
-VCF="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/6_Combine_intervals/Results/multisp/Melinaea_menophilus/snps_NO_setGT_GQ10.vcf.gz"
-POP_FILE="/mnt/scratch/projects/biol-specgen-2018/yacine/Conv_Evol/admixtools/f4/Inputs/species_samples.txt"
-P1=$1
-P2=$2
-P3=$3
-P4=$4
+##########################
+# Parse input parameters #
+##########################
+OPTIONS=$(getopt -o v:o:p: \
+    --long vcf:,outdir:,pop:,p1:,p2:,p3:,p4: \
+    -n "$0" -- "$@")
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+eval set -- "$OPTIONS"
+
+while true; do
+    case "$1" in
+        -v|--vcf) VCF="$2"; shift 2 ;;
+        -o|--outdir) OUTDIR="$2"; shift 2 ;;
+        -p|--pop) POP_FILE="$2"; shift 2 ;;
+        --p1) P1="$2"; shift 2 ;;
+        --p2) P2="$2"; shift 2 ;;
+        --p3) P3="$2"; shift 2 ;;
+        --p4) P4="$2"; shift 2 ;;
+        --) shift; break ;;
+        *) break ;;
+    esac
+done
+
+
+#################
+# Define prefix #
+#################
 PREFIX=${P1}_${P2}_${P3}_${P4}
-
+echo -e VCF file: "${VCF}"
+echo -e Pop file: "${POP_FILE}"
 echo -e P1:"${P1}"
 echo -e P2:"${P2}"
 echo -e P3:"${P3}"
 echo -e P4:"${P4}"
+echo Options parsed
 
-# Create phenotype file
-RESULTS="../Results/$PREFIX"
+#########################
+# Create phenotype file #
+#########################
+RESULTS="$OUTDIR/$PREFIX"
 mkdir -p $RESULTS
+echo -e The results are saved in: "${RESULTS}"
 
 if [ -f  $RESULTS/"$PREFIX"_phenotype_file.txt ]; then
     rm  $RESULTS/"$PREFIX"_phenotype_file.txt
@@ -35,18 +70,35 @@ for i in $P1 $P2 $P3 $P4; do cat ${POP_FILE} |awk -v pattern="$i" '$1 ~ pattern'
 for i in $P1 $P2 $P3 $P4; do echo $i >> $RESULTS/"$PREFIX"_subspecies.txt; done
 echo POP FILE READY
 
-# Create subsetted vcf file for taxa that will be analysed
-time bcftools view -S <(cat $RESULTS/"$PREFIX"_phenotype_file.txt|awk '{print $1}') $VCF| bcftools view -i 'F_MISSING < 0.2'| bcftools filter -e 'FORMAT/GQ < 10'| bcftools view -m2 -M2 -v snps -O z -o  $RESULTS/"$PREFIX".vcf.gz --threads 8
-tabix $RESULTS/"$PREFIX".vcf.gz
-
+####################################################
+# Create subsetted vcf with the 4 taxa of interest #
+####################################################
+echo Subsetting VCF 
+bcftools view -S <(awk '{print $2}' "$RESULTS/${PREFIX}_phenotype_file.txt") \
+  -i 'F_MISSING<0.2 && FORMAT/GQ>=10' \
+  -m2 -M2 -v snps \
+  -O z -o "$RESULTS/${PREFIX}.vcf.gz" \
+  --threads 8 "$VCF"
+  
+tabix "$RESULTS/${PREFIX}.vcf.gz"
+echo Subsetted VCF generated
 # load plink
-PLINK="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/plink_linux_x86_64_20231018/plink"
+#PLINK="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/plink_linux_x86_64_20231018/plink"
 
-# Run plink to generate a *bim *fam *bed files used as input by admitools2
-$PLINK --vcf $VCF --double-id --allow-extra-chr --allow-no-sex --set-missing-var-ids @:# --make-bed --out ../Inputs/subspecies 
-awk '{if(!($1 in seen)) seen[$1]=++counter; $1=seen[$1]; print}' OFS='\t' ../Inputs/subspecies.bim > ../Inputs/Inputs.bim
-mv ../Inputs/subspecies.bed ../Inputs/Inputs.bed
+###############################################
+# Run plink (output needed to run admixtools) #
+###############################################
+echo Running plink
+plink --vcf $VCF --double-id --allow-extra-chr --allow-no-sex --set-missing-var-ids @:# --make-bed --out $RESULTS/subspecies --threads 8
+echo Change contig names to numbers
+awk '{if(!($1 in seen)) seen[$1]=++counter; $1=seen[$1]; print}' OFS='\t' $RESULTS/subspecies.bim > $RESULTS/Inputs.bim
+mv $RESULTS/subspecies.bed $RESULTS/Inputs.bed
+echo Plink done
 
-# run the f4 using admixtools
-module load  R/4.2.1-foss-2022a
+##########################
+# Compute f4 statistics #
+#########################
+echo Start f4 statistics using admixtools
 Rscript ./f4.R $RESULTS/"$PREFIX"_subspecies.txt $RESULTS
+echo DONE
+
