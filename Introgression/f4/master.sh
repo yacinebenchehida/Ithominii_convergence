c24a828ea4c2fa87ab66e0edcf12eea221adc259
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2025-09-20 22:08:37 +0100

    Update master.sh

--- Introgression/f4/master.sh
+++ Introgression/f4/master.sh
@@ -1,7 +1,14 @@
-P1_list=(isocomma_simulator isocomma_simulator mothone_mothone menophilus_hicetas menophilus_ssp)
-P2_list=(isocomma_isocomma isocomma_isocomma mothone_messenina menophilus_ernestoi menophilus_ernestoi)
-P3_list=(menophilus_zaneka menophilus_menophilus menophilus_zaneka cydon mothone_mothone)
-P4_list=(menophilus_ernestoi menophilus_ernestoi menophilus_ernestoi maeolus mothone_messenina)
+#!/bin/bash
+
+# Define variables
+$VCF="/mnt/scratch/projects/biol-specgen-2018/yacine/Bioinformatics/6_Combine_intervals/Results/multisp/Melinaea_menophilus/snps_NO_setGT_GQ10.vcf.gz" # Must be bgzipped and indexed  
+$OUTPUT="/mnt/scratch/projects/biol-specgen-2018/yacine/Conv_Evol/admixtools/f4/Results"
+$POP_FILE="/mnt/scratch/projects/biol-specgen-2018/yacine/Conv_Evol/admixtools/f4/Inputs/species_samples.txt" # Tab separate two columns file (column 1: population; column 2: Sample_ID)
+
+P1_list=(pop11 pop12 pop13 pop14 pop15)
+P2_list=(pop21 pop22 pop23 pop24 pop25)
+P3_list=(pop31 pop32 pop33 pop34 pop35)
+P4_list=(pop41 pop42 pop43 pop44 pop45)
 
 for i in ${!P1_list[@]}; do
     P1=${P1_list[$i]}
@@ -9,5 +16,6 @@ for i in ${!P1_list[@]}; do
     P3=${P3_list[$i]}
     P4=${P4_list[$i]}
     
-    sbatch ./f4.sh ${P1} ${P2} ${P3} ${P4}
+    sbatch ./f4.sh -v $VCF -o $OUTPUT_PATH -p $POP_FILE --p1 ${P1} --p2 ${P2} --p3 ${P3} --p4 ${P4}   
 done
+
