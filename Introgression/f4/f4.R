1880cbf0dbd427dd6d654c0f25aa630924341c0c
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2025-09-23 15:24:07 +0100

    Update f4.R

--- Introgression/f4/f4.R
+++ Introgression/f4/f4.R
@@ -3,16 +3,18 @@ library(tidyverse)
 
 args <- commandArgs(trailingOnly = TRUE)
 subspecies_files <- args[1]
-data = args[2]
+data_plink = paste(args[2],"/Input",sep="")
+results = args[2]
+
 pops <- read.table(subspecies_files)
 pop1 <- pops[1,1]
 pop2 <- pops[2,1]
 pop3 <- pops[3,1]
 pop4 <- pops[4,1]
-outfile = paste(args[2],"/Input/",pop1,"_",pop2,"_",pop3,"_",pop4,".txt", sep="")
+outfile = paste(results,"/",pop1,"_",pop2,"_",pop3,"_",pop4,".txt", sep="")
 print(outfile)
 
-out = f4(data, pop1, pop2, pop3, pop4)
+out = f4(data_plink, pop1, pop2, pop3, pop4)
 
 write_tsv(out, outfile)
 
