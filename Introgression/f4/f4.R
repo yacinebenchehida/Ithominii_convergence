6145193397a04bdb1d8a394fb580152e2810e4ba
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2025-09-23 15:16:53 +0100

    Update f4.R

--- Introgression/f4/f4.R
+++ Introgression/f4/f4.R
@@ -9,7 +9,7 @@ pop1 <- pops[1,1]
 pop2 <- pops[2,1]
 pop3 <- pops[3,1]
 pop4 <- pops[4,1]
-outfile = paste(args[2],"/",pop1,"_",pop2,"_",pop3,"_",pop4,".txt", sep="")
+outfile = paste(args[2],"/Input/",pop1,"_",pop2,"_",pop3,"_",pop4,".txt", sep="")
 print(outfile)
 
 out = f4(data, pop1, pop2, pop3, pop4)
